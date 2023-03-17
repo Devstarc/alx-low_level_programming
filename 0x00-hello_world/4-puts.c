a539f8dcc469a0e08f6ee237a2b1fe121d71965b
Israel Joseph <israeljoseph125@gmail.com>
2023-03-16 21:16:41 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+* Main - A programm that prints a line with puts function
+*Return - Returns 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
