bd2bf76d1a513889f3b93ff46547fa18819cc4e0
Israel Joseph <israeljoseph125@gmail.com>
2023-03-16 22:26:05 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unisted.h>
+/**
+ * Main - A programm that prints errors
+* Return - 0(Success)
+*/
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
