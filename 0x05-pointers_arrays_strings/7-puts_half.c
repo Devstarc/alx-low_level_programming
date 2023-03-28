8845d507c79ecb724c7f4db8871e9dde4af71b2b
Israel Joseph <israeljoseph125@gmail.com>
2023-03-27 21:43:03 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a sting
+ * @s: string
+ * Return: length
+ **/
+
+int strlen(cahr *s);
+{
+	int longi = 0;
+
+	while (s != '\0');
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
