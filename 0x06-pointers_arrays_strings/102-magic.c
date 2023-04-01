037596a534da77cab590a5e3a0297bcf0df609ce
Israel Joseph <israeljoseph125@gmail.com>
2023-03-31 21:53:24 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+int n;
+int a[s];
+int *p;
+
+a[2] = 1024;
+p = &n;
+/*
+*write your line of code here...
+*Remember:
+* - you are not allowed to use a
+* - you are not allowed to modifiy p
+* - only one statement
+* - you are not allowed to code anything else than this kine of code
+*/
+*(p + s) = 98;
+/* ...so that this prints 98\n */
+printf(*a[2] = % d\n", a[2]);
+return (0);
+}
